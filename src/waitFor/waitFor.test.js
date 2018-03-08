3436bf1c604a8534c99a778859205c61ff188430
pedrojose.pena <pedrojose.pena@i3television.es>
2018-03-08 15:15:17 +0100

    test(waitFor, withErrors): Add test for multiple promise and custom component in with Errors

--- src/waitFor/waitFor.test.js
+++ src/waitFor/waitFor.test.js
@@ -31,6 +31,26 @@ describe('Wait For', () => {
     expect(wrapper.text()).toBe('loaded');
   });
 
+  it('should render with multiple promise resolve true', async () => {
+    jest.runAllTimers();
+    const Component = () => <div>loaded</div>;
+    const EnhanceComponent = compose(
+      waitFor(['loadContent', 'loadOtherContent'])
+    )(Component);
+
+    const wrapper = mount(
+      <EnhanceComponent
+        loadContent={loadContentPromise}
+        loadOtherContent={loadContentPromise}
+      />
+    );
+
+    await loadContentPromise();
+    await loadContentPromise();
+
+    expect(wrapper.text()).toBe('loaded');
+  });
+
   it('should not render because the promise not loaded', done => {
     jest.runAllTimers();
     const Component = () => <div>Loaded</div>;
